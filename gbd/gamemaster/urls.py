fa32d25ef43b7622a8660a1d7c3d256845c2a41b
Vu H. Chu-Le <vu@minerva.kgi.edu>
2024-03-22 21:50:25 -0700

    modified:   gbd/gamemaster/urls.py

--- gbd/gamemaster/urls.py
+++ gbd/gamemaster/urls.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from django.urls import path
 
 from gbd.gamemaster.views import latest_view, update
@@ -5,7 +7,5 @@ from gbd.gamemaster.views import latest_view, update
 app_name = "gamemaster"
 urlpatterns = [
     path("latest/", view=latest_view, name="latest"),
-    path("update/", view=update),
-]
     path("update/", view=update),
 ]
